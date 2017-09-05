0f44b29e0f3955233c73f2cffebf64e93cad6334
Emory Merryman <emory.merryman@gmail.com>
2017-09-05 17:08:53 +0000

    start and test registry

--- image/root/entrypoint.sh
+++ image/root/entrypoint.sh
@@ -2,4 +2,11 @@
 
 docker login --username ${DOCKERHUB_USERNAME} --password ${DOCKERHUB_PASSWORD} &&
     docker pull endlessplanet/registry &&
+    docker network create special &&
+    docker container --name registry --env KEY --env CERT endlessplanet/registry &&
+    docker network connect --alias registry special registry &&
+    docker container start registry &&
+    docker image pull alpine:3.4 &&
+    docker image tag alpine:3.4 registry/my-alpine2:1 &&
+    docker push registry/my-alpine2:1
     bash
