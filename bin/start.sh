3b9eefa72b259218f5605fa31099384b281267e4
Emory Merryman <emory.merryman@gmail.com>
2017-09-05 13:39:06 +0000

    add network spec

--- bin/start.sh
+++ bin/start.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
-export NETWORK="${3}" &&
-    docker \
-        container \
-        run \
-        --interactive \
-        --tty \
-        --rm \
-        --volume /var/run/docker.sock:/var/run/docker.sock:ro \
-        --workdir /home/user \
-        --network ${NETWORK} \
-        --env NETWORK \
-        --env USERNAME="${1}" \
-        --env EMAIL="${2}" \
-        --env ID_RSA="$(cat ~/.ssh/id_rsa)" \
-        --env KNOWN_HOSTS="$(cat ~/.ssh/known_hosts)" \
-        endlessplanet/client
+docker \
+    container \
+    run \
+    --interactive \
+    --tty \
+    --rm \
+    --volume /var/run/docker.sock:/var/run/docker.sock:ro \
+    --workdir /home/user \
+    --network ${NETWORK} \
+    --env NETWORK \
+    --env USERNAME="${1}" \
+    --env EMAIL="${2}" \
+    --env ID_RSA="$(cat ~/.ssh/id_rsa)" \
+    --env KNOWN_HOSTS="$(cat ~/.ssh/known_hosts)" \
+    endlessplanet/client
