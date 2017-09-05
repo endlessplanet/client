217f9aa6604625d28d9136739ce9ba5033393fc8
Emory Merryman <emory.merryman@gmail.com>
2017-09-05 17:02:07 +0000

    get rid of ssh crap

--- image/root/entrypoint.sh
+++ image/root/entrypoint.sh
@@ -1,20 +1,5 @@
 #!/bin/sh
 
-if [ ! -d ${HOME}/.ssh ]
-then
-    mkdir ${HOME}/.ssh &&
-        chmod 0700 ${HOME}/.ssh
-fi &&
-    if [ ! -d ${HOME}/.ssh/id_rsa ]
-    then
-        echo "${ID_RSA}" > ${HOME}/.ssh/id_rsa &&
-            chmod 0600 ${HOME}/.ssh/id_rsa
-    fi &&
-    if [ ! -d ${HOME}/.ssh/known_hosts ]
-    then
-        echo "${KNOWN_HOSTS}" > ${HOME}/.ssh/known_hosts &&
-            chmod 0600 ${HOME}/.ssh/known_hosts
-    fi &&
-    echo docker login --username ${DOCKERHUB_USERNAME} --password ${DOCKERHUB_PASSWORD} &&
-    echo docker pull endlessplanet/registry &&
+docker login --username ${DOCKERHUB_USERNAME} --password ${DOCKERHUB_PASSWORD} &&
+    docker pull endlessplanet/registry &&
     bash
