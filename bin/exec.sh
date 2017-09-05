244f39b63af750a1b997f76b2945daa4e0d5d347
Emory Merryman <emory.merryman@gmail.com>
2017-09-05 15:58:44 +0000

    fix exec script

--- bin/exec.sh
+++ bin/exec.sh
@@ -4,9 +4,9 @@ NETWORK=$(mktemp) &&
     DIND=$(mktemp) &&
     CLIENT=$(mktemp) &&
     cleanup() {
-        docker container stop ${DIND} ${CLIENT} &&
-            docker container rm --force --volumes ${DIND} ${CLIENT} &&
-            docker network rm ${NETWORK} &&
+        docker container stop $(cat ${DIND}) $(cat ${CLIENT}) &&
+            docker container rm --force --volumes $(cat ${DIND}) $(cat ${CLIENT}) &&
+            docker network rm $(cat ${NETWORK}) &&
             rm -f ${NETWORK} ${DIND} ${CLIENT}
     } &&
     trap cleanup EXIT &&
@@ -21,6 +21,7 @@ NETWORK=$(mktemp) &&
         --host tcp://0.0.0.0:2376 &&
     docker network connect --alias docker $(cat ${NETWORK}) $(cat ${DIND}) &&
     docker container start $(cat ${DIND}) &&
+    rm -f ${CLIENT} &&
     docker \
         container \
         create \
@@ -35,4 +36,4 @@ NETWORK=$(mktemp) &&
         --env DOCKER_HOST="tcp://docker:2376" \
         endlessplanet/client &&
     docker network connect $(cat ${NETWORK}) $(cat ${CLIENT}) &&
-    docker container start --interactive $(cat ${CID_FILE})
+    docker container start --interactive $(cat ${CLIENT})
