2947f6b196e29d693f761a5d02fc04870153085a
Emory Merryman <emory.merryman@gmail.com>
2017-09-05 17:30:31 +0000

    better test local registry

--- image/root/entrypoint.sh
+++ image/root/entrypoint.sh
@@ -3,12 +3,21 @@
 docker login --username ${DOCKERHUB_USERNAME} --password ${DOCKERHUB_PASSWORD} &&
     # docker pull endlessplanet/registry &&
     docker network create special &&
-    docker container create --name registry --env KEY --env CERT --publish 5000:5000 --publish 80:80 registry:2 &&
+    docker \
+        container \
+        create \
+        --name registry \
+        --publish 5000:5000 \
+        registry:2 &&
     docker network connect --alias registry special registry &&
     docker container start registry &&
     docker image pull alpine:3.4 &&
     docker image tag alpine:3.4 localhost:5000/my-alpine3:1 &&
     docker image push localhost:5000/my-alpine3:1 &&
+    docker image rm alpine:3.4 localhost:5000/my-alpine3:1 &&
+    docker image ls &&
+    docker image pull localhost:5000/my-alpine3:1 &&
+    docker image ls &&
     # docker image tag alpine:3.4 registry/my-alpine2:1 &&
     # docker image push registry/my-alpine2:1 &&
     bash
