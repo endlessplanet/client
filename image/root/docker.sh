12011ef1ca3f4c7c21ac62c2ddcdcdb8aeb83f8e
Emory Merryman <emory.merryman@gmail.com>
2017-09-05 15:14:03 +0000

    use host tag

--- image/root/docker.sh
+++ image/root/docker.sh
@@ -2,6 +2,6 @@
 
 CID_FILE=$(mktemp ${HOME}/docker/containers/XXXXXXXX) &&
     rm -f ${CID_FILE} &&
-    sudo --preserve-env docker container create --cidfile ${CID_FILE} --privileged docker:edge "${@}" &&
-    sudo --preserve-env docker network connect --link docker $(cat ${HOME}/docker/networks/default) $(cat ${CID_FILE}) &&
-    sudo --preserve-env docker container start --interactive $(cat ${CID_FILE})
+    sudo --preserve-env docker --host tcp://docker:2376 container --host tcp:create --cidfile ${CID_FILE} --privileged docker:edge "${@}" &&
+    sudo --preserve-env docker --host tcp://docker:2376 network connect --link docker $(cat ${HOME}/docker/networks/default) $(cat ${CID_FILE}) &&
+    sudo --preserve-env docker --host tcp://docker:2376 container start --interactive $(cat ${CID_FILE})
