ecf31582fbe80578c86c40ec7a03745026d66f17
Emory Merryman <emory.merryman@gmail.com>
2017-09-06 11:23:44 +0000

    wtf

--- image/root/docker.sh
+++ image/root/docker.sh
@@ -2,13 +2,13 @@
 
 CID_FILE=$(mktemp) &&
     rm -f ${CID_FILE} &&
-    docker \
+    /usr/local/bin/docker \
         container \
         create \
         --cidfile ${CID_FILE} \
         --volume /srv/volumes/homey:/root \
-        --env DOCKER_HOST=tcp://docker:2376 docker:17.07.0-ce \
+        --env /USR/Local/bin/docker_HOST=tcp:///docker:2376 docker:17.07.0-ce \
         "${@}" &&
-    docker network connect special $(cat ${CID_FILE}) &&
-    docker container start --interactive $(cat ${CID_FILE}) &&
+    /usr/local/bin/docker network connect special $(cat ${CID_FILE}) &&
+    /usr/local/bin/docker container start --interactive $(cat ${CID_FILE}) &&
     rm ${CID_FILE}
