dee76b2ca80e5f0d256d00b9830a8fef367d2baa
Emory Merryman <emory.merryman@gmail.com>
2017-09-06 03:28:43 +0000

    it actually works

--- image/root/entrypoint.sh
+++ image/root/entrypoint.sh
@@ -4,6 +4,15 @@ docker login --username ${DOCKERHUB_USERNAME} --password ${DOCKERHUB_PASSWORD} &
     docker volume create auth &&
     docker network create special &&
     sudo mkdir /srv/volumes/auth &&
+    docker \
+        container \
+        create \
+        --name dind \
+        --privileged \
+        docker:17.07.0-ce-dind \
+        --host tcp://0.0.0.0:2376 --insecure-registry registry:443 &&
+    docker network connect --alias docker special dind &&
+    docker container start dind &&
     docker container create --name genpass --interactive --entrypoint htpasswd registry:2.6.2 -Bnb user password &&
     docker container start --interactive genpass | sudo tee /srv/volumes/auth/htpasswd &&
     sudo mkdir /srv/volumes/certs &&
@@ -36,17 +45,32 @@ EOF
         registry:2.6.2 &&
     docker network connect --alias registry special registry &&
     docker container start registry &&
-    docker image pull alpine:3.4 &&
-    docker image tag alpine:3.4 registry:443/it/my-alpine3:1 &&
-    docker container create --volume /srv/volumes/homey:/root --name login --volume /var/run/docker.sock:/var/run/docker.sock:ro docker:17.07.0-ce login --username user --password password https://registry:443 &&
+    docker container create --volume /srv/volumes/homey:/root --name login2 --volume /var/run/docker.sock:/var/run/docker.sock:ro --env DOCKER_HOST=tcp://docker:2376 docker:17.07.0-ce login --username ${DOCKERHUB_USERNAME} --password ${DOCKERHUB_PASSWORD} &&
+    docker network connect special login2 &&
+    echo ALPHA 0100 &&
+    docker container start --interactive login2 &&
+    echo ALPHA 0101 &&
+    docker container create --volume /srv/volumes/homey:/root --name pull2 --volume /var/run/docker.sock:/var/run/docker.sock:ro --env DOCKER_HOST=tcp://docker:2376 docker:17.07.0-ce image pull alpine:3.4 &&
+    docker network connect special pull2 &&
+    docker container start --interactive pull2 &&
+    docker container create --volume /srv/volumes/homey:/root --name tag2 --volume /var/run/docker.sock:/var/run/docker.sock:ro --env DOCKER_HOST=tcp://docker:2376 docker:17.07.0-ce image tag alpine:3.4 registry:443/it/my-alpine3:1 &&
+    docker network connect special tag2 &&
+    docker container start --interactive tag2 &&
+    docker container create --volume /srv/volumes/homey:/root --name login --volume /var/run/docker.sock:/var/run/docker.sock:ro --env DOCKER_HOST=tcp://docker:2376 docker:17.07.0-ce login --username user --password password https://registry:443 &&
     docker network connect special login &&
     docker container start --interactive login &&
-    docker container create --volume /srv/volumes/homey:/root --name push-alpine --volume /var/run/docker.sock:/var/run/docker.sock:ro docker:17.07.0-ce image push registry:443/it/my-alpine3:1 &&
+    docker container create --volume /srv/volumes/homey:/root --name push-alpine --volume /var/run/docker.sock:/var/run/docker.sock:ro --env DOCKER_HOST=tcp://docker:2376 docker:17.07.0-ce image push registry:443/it/my-alpine3:1 &&
     docker network connect special push-alpine &&
     docker container start --interactive push-alpine &&
-    docker image ls &&
-    docker image rm alpine:3.4 registry:443/it/my-alpine3:1 &&
-    docker container create --volume /srv/volumes/homey:/root --name pull-alpine --volume /var/run/docker.sock:/var/run/docker.sock:ro docker:17.07.0-ce image pull registry:443/it/my-alpine3:1 &&
+    docker container create --volume /srv/volumes/homey:/root --name ls21 --volume /var/run/docker.sock:/var/run/docker.sock:ro --env DOCKER_HOST=tcp://docker:2376 docker:17.07.0-ce docker image ls &&
+    docker network connect special ls21 &&
+    docker container start --interactive ls21 &&
+    docker container create --volume /srv/volumes/homey:/root --name rm2 --volume /var/run/docker.sock:/var/run/docker.sock:ro --env DOCKER_HOST=tcp://docker:2376 docker:17.07.0-ce docker image rm alpine:3.4 registry:443/it/my-alpine3:1 &&
+    docker network connect special rm2 &&
+    docker container start --interactive rm2 &&
+    docker container create --volume /srv/volumes/homey:/root --name pull-alpine --volume /var/run/docker.sock:/var/run/docker.sock:ro --env DOCKER_HOST=tcp://docker:2376 docker:17.07.0-ce image pull registry:443/it/my-alpine3:1 &&
     docker network connect special pull-alpine &&
     docker container start --interactive pull-alpine &&
-    docker image ls
+    docker container create --volume /srv/volumes/homey:/root --name ls22 --volume /var/run/docker.sock:/var/run/docker.sock:ro --env DOCKER_HOST=tcp://docker:2376 docker:17.07.0-ce docker image ls &&
+    docker network connect special ls22 &&
+    docker container start --interactive ls22
