fc312758e011f7fc58db631f9c32f1f09b52a579
Emory Merryman <emory.merryman@gmail.com>
2017-09-06 11:31:30 +0000

    use home bin docker

--- image/root/entrypoint.sh
+++ image/root/entrypoint.sh
@@ -6,7 +6,7 @@ sh /opt/docker/setup-docker.sh &&
     ${HOME}/bin/docker image tag alpine:3.4 registry:443/it/my-alpine3:1 &&
     ${HOME}/bin/docker login --username user --password password https://registry:443 &&
     ${HOME}/bin/docker image push registry:443/it/my-alpine3:1 &&
-    ${HOME}/bin/docker docker image ls &&
-    ${HOME}/bin/docker docker image rm alpine:3.4 registry:443/it/my-alpine3:1 &&
+    ${HOME}/bin/docker image ls &&
+    ${HOME}/bin/docker image rm alpine:3.4 registry:443/it/my-alpine3:1 &&
     ${HOME}/bin/docker image pull registry:443/it/my-alpine3:1 &&
-    ${HOME}/bin/docker docker image ls
+    ${HOME}/bin/docker image ls
