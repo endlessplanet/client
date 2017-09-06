e76ec1b0624128fa15b7ba53b0b9b243724fd824
Emory Merryman <emory.merryman@gmail.com>
2017-09-06 10:57:36 +0000

    remove docker binding because we are using DOCKER_HOST env var

--- image/root/docker.sh
+++ image/root/docker.sh
@@ -2,7 +2,13 @@
 
 CID_FILE=$(mktemp) &&
     rm -f ${CID_FILE} &&
-    docker container create --cidfile ${CID_FILE} --volume /srv/volumes/homey:/root --volume /var/run/docker.sock:/var/run/docker.sock:ro --env DOCKER_HOST=tcp://docker:2376 docker:17.07.0-ce "${@}" &&
+    docker \
+        container \
+        create \
+        --cidfile ${CID_FILE} \
+        --volume /srv/volumes/homey:/root \
+        --env DOCKER_HOST=tcp://docker:2376 docker:17.07.0-ce \
+        "${@}" &&
     docker network connect special $(cat ${CID_FILE}) &&
     docker container start --interactive $(cat ${CID_FILE}) &&
     rm ${CID_FILE}
